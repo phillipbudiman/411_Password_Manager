2fd66afe9b43647a70cad3b2979a0f68c44bf516
pbudiman <Phillipbudiman@gmail.com>
2021-11-23 13:26:46 -0500

    Removed unnecessary import (JSONArray)

--- src/JSON_Writer.java
+++ src/JSON_Writer.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class JSON_Writer
