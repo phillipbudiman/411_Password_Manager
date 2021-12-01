42422ce331099a6265d82f77daa4440faf0e3d80
kaquoc <66937211+kaquoc@users.noreply.github.com>
2021-11-30 21:38:07 -0500

    remove some print statements in AES_file, ongoing adjustment in AES_test

--- src/AEStest.java
+++ src/AEStest.java
@@ -5,21 +5,31 @@ public class AEStest {
 	
 	
 	public static void main(String[] args) {
-		AES_file_encryption myobj = new AES_file_encryption("password123");
-		File myFile = new File("decryptfile.txt");
-		if (myobj.encrypt(myFile)) {
+
+		File myFile = new File("creds.json");
+		//instantiate encryption class
+		AES_file_encryption my_encrypt = new AES_file_encryption("password123");
+
+		//instantiate JSON classes
+		JSON_Reader J_read = new JSON_Reader();
+		JSON_Writer J_write = new JSON_Writer();
+
+		J_write.addEntry("www.github.com","username","password123","123");
+
+
+
+		if (my_encrypt.encrypt(myFile)) {
 			System.out.println("encryption success");
-		}else{
+		}else {
 			System.out.println("encryption fail");
 		}
-		/*
-		if (myobj.decrypt(myFile)) {
+
+		if (my_encrypt.decrypt(myFile)) {
 			System.out.println("decryption success");
-		}else{
+		}else {
 			System.out.println("decryption fail");
 		}
-		*/
-		
+
 		
 		
 	}
