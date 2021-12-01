03b7fb168aeb302b7d6b71c39f2a44624c2a8bad
kaquoc <66937211+kaquoc@users.noreply.github.com>
2021-12-01 00:57:21 -0500

    add J_write export to AES_test

--- src/AEStest.java
+++ src/AEStest.java
@@ -16,13 +16,15 @@ public class AEStest {
 
 		//writing into "creds.json" file  - return void
 		J_write.addEntry("www.github.com","username","password123");
+		J_write.export("creds");
+
 
 		//reading "creds.json" file
 		J_read.read("creds.json");   // - return ArrayList
 
 		//call encryption on "creds.json"
-		my_encrypt.encrypt(myFile);  //return true if success else false
-		my_encrypt.decrypt(myFile);  //return true if success else false
+		//my_encrypt.encrypt(myFile);  //return true if success else false
+		//my_encrypt.decrypt(myFile);  //return true if success else false
 		
 		
 	}
