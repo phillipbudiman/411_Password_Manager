bc4fa8f751b2a1f3278f57fd64cf6a6556239752
NatarBL <94152894+NatarBL@users.noreply.github.com>
2021-12-03 15:48:42 -0500

    Update SceneController.java
    -Added password generating functionality. 
    -Removed the need for vault.fxml
    -Created the "login" method that merges the front and back end.

--- src/SceneController.java
+++ src/SceneController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
@@ -40,24 +41,90 @@ public class SceneController {
     @FXML
     TextField PasswordLength;
 
-    public void displayPassword(){
-
-        passwordDisplay.setText("nsnvohjnt");
-
+    public String passwordGenerator(int len, boolean number, boolean upper, boolean special){
         Random rand = new Random();
 
-        boolean numbersBool= Numbers.isSelected();
+        int passwordLengthInt = len;
+        String randPassword = "";
+
+        for (int i=0; i < passwordLengthInt; i++){
+            if (number==true && upper==false && special==false) {
+                if (rand.nextInt(2) == 0) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+                }else{
+                    randPassword = randPassword + (char) (rand.nextInt(10) + 48);
+                }
+            }
+            else if (number==false && upper==true && special==false) {
+                if (rand.nextInt(2) == 0) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+                }else{
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 65);
+                }
+            }
+            else if (number==false && upper==false && special==true) {
+                if (rand.nextInt(2) == 0) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+                }else{
+                    randPassword = randPassword + (char) (rand.nextInt(4) + 35);
+                }
+            }
+            else if (number==true && upper==true && special==false) {
+                if (rand.nextInt(3) == 0) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+                }else if (rand.nextInt(3) == 1) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 65);
+                }else{
+                    randPassword = randPassword + (char) (rand.nextInt(10) + 48);
+                }
+            }
+            else if (number==true && upper==true && special==true) {
+                if (rand.nextInt(4) == 0) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+                }else if (rand.nextInt(4) == 1) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 65);
+                }else if (rand.nextInt(4) == 3) {
+                    randPassword = randPassword + (char) (rand.nextInt(4 ) + 35);
+                }else{
+                    randPassword = randPassword + (char) (rand.nextInt(10) + 48);
+                }
+            }
+            else if (number==true && upper==false && special==true) {
+                if (rand.nextInt(3) == 0) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+                }else if (rand.nextInt(3) == 1) {
+                    randPassword = randPassword + (char) (rand.nextInt(4 ) + 35);
+                }else{
+                    randPassword = randPassword + (char) (rand.nextInt(10) + 48);
+                }
+            }
+            else if (number==false && upper==true && special==true) {
+                if (rand.nextInt(3) == 0) {
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+                }else if (rand.nextInt(3) == 1) {
+                    randPassword = randPassword + (char) (rand.nextInt(4 ) + 35);
+                }else{
+                    randPassword = randPassword + (char) (rand.nextInt(26) + 65);
+                }
+            }
+            else{
+                randPassword = randPassword + (char) (rand.nextInt(26) + 97);
+            }
+        }
+        return randPassword;
+    }
+
+    public void displayPassword(){
+
+        String passwordLength = PasswordLength.getText();
+        int passwordLengthInt = Integer.parseInt(passwordLength);
+
+        boolean numbersBool = Numbers.isSelected();
         boolean uppercaseLettersBool = UppercaseLetters.isSelected();
         boolean specialCharactersBool = SpecialCharacters.isSelected();
-        int passwordLengthInt = Integer.parseInt(PasswordLength.getText());
-        /**
-        if (numbersBool == false && uppercaseLettersBool == false && specialCharactersBool == false){
-            for (int i=0; i <passwordLengthInt; i++){
-            }
-            passwordDisplay.setText("nsnvohjn");
-        }**/
-        //Random rand = new Random();
 
+        String randPassword = passwordGenerator(passwordLengthInt,numbersBool,uppercaseLettersBool,specialCharactersBool);
+        passwordDisplay.setText(randPassword);
     }
 
     @FXML
@@ -74,20 +141,53 @@ public class SceneController {
     @FXML
     Button LoginButton;
 
+    @FXML
+    TextArea VaultText;
+    String vaultString;
+
     public void login(ActionEvent event) throws IOException{
+
+        //Create user login-database
+        User user_ka = new User("ka411", "kaPW");
+        user_ka.storeUser();
+
+        User user_phil = new User("phil411", "philPW");
+        user_phil.storeUser();
+
+        User user_nate = new User("nate411", "natePW");
+        user_nate.storeUser();
 
         username = UsernameLogin.getText();
         masterPassword = PasswordLogin.getText();
 
-        if (username.equals("User42") && masterPassword.equals("ILoveCS")){
-            Parent root = FXMLLoader.load(getClass().getResource("Vault.fxml"));
-            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-            scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
+        if (username.equals(user_ka.getLogin()) && masterPassword.equals(user_ka.getPassword())){
+            user_ka.encrypt_vault();
+            user_ka.decrypt_vault();
+            vaultString = user_ka.displayUserVault("creds.json");
+            VaultText.setText(vaultString);
+
+        }
+        else if (username.equals(user_phil.getLogin()) && masterPassword.equals(user_phil.getPassword())){
+            user_phil.encrypt_vault();
+            user_phil.decrypt_vault();
+            vaultString = user_phil.displayUserVault("creds2.json");
+            VaultText.setText(vaultString);
+
+        }
+        else if (username.equals(user_nate.getLogin()) && masterPassword.equals(user_nate.getPassword())){
+            user_nate.encrypt_vault();
+            user_nate.decrypt_vault();
+            vaultString = user_nate.displayUserVault("creds3.json");
+            VaultText.setText(vaultString);
+
         }
         else{
-            PasswordError.setText("Incorrect password.");
+            if (username.equals(user_ka.getLogin()) || username.equals(user_nate.getLogin()) || username.equals(user_phil.getLogin())) {
+                VaultText.setText("Incorrect login for " + username + ".");
+            }
+            else{
+                VaultText.setText("User does not exist.");
+            }
         }
     }
 
